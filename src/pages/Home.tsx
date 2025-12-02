ca3e3b213494cf8f6b1ec516bc9cf39220226bb2
Andrech <andrech1406@gmail.com>
2025-12-02 15:40:10 +0300

    add base map

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,20 @@
 import type { FC } from "react";
+import Map from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
 
 const Home: FC = () => {
-  return <div>Home page</div>;
+  const apiKey = import.meta.env.VITE_MAP_API_KEY;
+
+  return (
+    <Map
+      initialViewState={{
+        longitude: 13,
+        latitude: 51,
+        zoom: 4,
+      }}
+      style={{ width: "100vw", height: "100vh" }}
+      mapStyle={`https://api.maptiler.com/maps/streets/style.json?key=${apiKey}`}
+    />
+  );
 };
 export default Home;
